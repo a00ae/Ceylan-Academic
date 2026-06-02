932654101aba7f7f02937dd985ff5a0ed1663d29
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-03 00:51:10 +0300

    add header

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,26 @@
+import styles from "./Header.module.scss";
+// إذا كنت تستخدم Vite، أضف ?react في نهاية المسار لاستيرادها كمكون
+import CeylanLogo from "../../../assets/logo/Ceylan-Language-Academi-Logo.svg?react";
+
+const header: string[] = ["home", "price", "content", "about my"];
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={styles["header_icon"]}>
+        <CeylanLogo className={styles.mainLogo} title="Ceylan Logo" />
+      </nav>
+      <ul className={styles["header_nav"]}>
+        {header.map((ele) => (
+          <li key={ele}>
+            <a data-element={ele} href={`#${ele}`}>
+              <span>{ele}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import svgr from "vite-plugin-svgr"; // 1. استيراد الإضافة
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Ceylan-Academic/",
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [
+    react(),
+    svgr({
+      // هذا الخيار يضمن معاملة ملفات السيرفر الملحقة بـ ?react كمكونات ريأكت
+      include: "**/*.svg?react",
+    }),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
   server: { port: 3000 },
 });
