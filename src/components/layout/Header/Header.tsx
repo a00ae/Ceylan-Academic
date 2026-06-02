557347cb18ebae236b61076ec7ba882009590266
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-03 00:53:16 +0300

    add header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,5 +1,6 @@
 import styles from "./Header.module.scss";
 // إذا كنت تستخدم Vite، أضف ?react في نهاية المسار لاستيرادها كمكون
+// @ts-ignore
 import CeylanLogo from "../../../assets/logo/Ceylan-Language-Academi-Logo.svg?react";
 
 const header: string[] = ["home", "price", "content", "about my"];
