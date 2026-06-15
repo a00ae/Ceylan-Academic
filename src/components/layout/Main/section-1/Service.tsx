e8f71a5303c1729c505f5b31eaa4ff006c4d488c
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-15 18:53:44 +0300

    new imges

--- src/components/layout/Main/section-1/Service.tsx
+++ src/components/layout/Main/section-1/Service.tsx
@@ -1,9 +1,10 @@
 // import { SERVICE_IMAGES } from "";
-import { SERVICE_IMAGES } from "@/assets/service/imges";
-import ceylan from "@/assets/service/ceylan.drawio.svg";
+// import { SERVICE_IMAGES } from "@/assets/service/imges";
+import ceylan from "@/assets/service/dil.drawio.svg";
 import ig from "@/assets/service/ig.drawio.svg";
 import { BentoGrid } from "../../../ui/Card/BentoGrid";
 import styles from "./Service.module.scss";
+
 const Service = () => {
   return (
     <section className={styles["service"]}>
@@ -17,14 +18,50 @@ const Service = () => {
         </div>
         <div className={styles["service_img"]}>
           <div className={styles["service_img_box-img"]}>
-            <img className={styles["ceylan"]} src={ceylan} alt="dil kurs" />
-            <img className={styles["ig"]} src={ig} alt="dil kurs" />
-            <img className={styles["ar-primary"]} draggable={false} src={SERVICE_IMAGES.ar.primary} alt="Arapça öğrenin" />
-            <img className={styles["ar-secondary"]} draggable={false} src={SERVICE_IMAGES.ar.secondary} alt="Arapça öğrenin" />
-            <img className={styles["en-secondary"]} draggable={false} src={SERVICE_IMAGES.en.secondary} alt="Arapça öğrenin" />
-            <img className={styles["en-primary"]} draggable={false} src={SERVICE_IMAGES.en.primary} alt="Arapça öğrenin" />
-            <img className={styles["ar-secondary"]} draggable={false} src={SERVICE_IMAGES.fr.secondary} alt="Arapça öğrenin" />
-            <img className={styles["ar-primary"]} draggable={false} src={SERVICE_IMAGES.fr.primary} alt="Arapça öğrenin" />
+            <div className={styles["container_img"]}>
+              <div className={styles["ceylan-dil"]}>
+                <img className={styles["ceylan"]} src={ceylan} alt="dil kurs" />
+                <img className={styles["ig"]} src={ig} alt="dil kurs" />
+              </div>
+              {/* <div className={styles["dil"]}>
+                <img
+                  className={styles["ar-primary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.ar.primary}
+                  alt="Arapça öğrenin"
+                />
+                <img
+                  className={styles["ar-secondary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.ar.secondary}
+                  alt="Arapça öğrenin"
+                />
+                <img
+                  className={styles["en-secondary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.en.secondary}
+                  alt="Arapça öğrenin"
+                />
+                <img
+                  className={styles["en-primary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.en.primary}
+                  alt="Arapça öğrenin"
+                />
+                <img
+                  className={styles["fr-secondary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.fr.secondary}
+                  alt="Arapça öğrenin"
+                />
+                <img
+                  className={styles["fr-primary"]}
+                  draggable={false}
+                  src={SERVICE_IMAGES.fr.primary}
+                  alt="Arapça öğrenin"
+                />
+              </div> */}
+            </div>
           </div>
           <div className={styles["service_img_specifications"]}>
             <div className={styles["service_specifications-desc"]}>
@@ -32,7 +69,6 @@ const Service = () => {
               temellerini öğrenin.
             </div>
             <BentoGrid />
-
           </div>
         </div>
       </div>
