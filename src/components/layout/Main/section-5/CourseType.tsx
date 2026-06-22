d19d3a652fef93e7969673a55b1ed466e8e8a116
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-22 16:09:18 +0300

    add new card ask

--- src/components/layout/Main/section-5/CourseType.tsx
+++ src/components/layout/Main/section-5/CourseType.tsx
@@ -3,16 +3,29 @@ import styles from "./Course-type.module.scss";
 import img from "@/assets/about-us/about-us-ceylan-dil.webp";
 import Box from "@/components/ui/Card/Box-Heading/Box";
 import Heading from "@/components/ui/Card/Box-Heading/Heading";
+import { RiArrowUpSLine } from "@remixicon/react";
+import { useState } from "react";
+import { ASK_DATA } from "./dataAsk";
 
-type Props = {};
+type Props = {
+  id: number;
+  active: boolean;
+};
 
 const CourseType = () => {
   const { courseType } = pageData;
+  const [isActive, setIsActive] = useState<Props | null>(null);
+  const handleAccordion = ({ id, active }: Props) => {
+    setIsActive({ id: id, active: !active });
+  };
   return (
     <section className={styles["course-type"]}>
       <div className={styles["course-type_container"]}>
         <div className={styles["course-type_left"]}>
-          <img src={img} alt="" />
+          <p>(0{isActive === null ?  "1" : isActive?.id })</p>
+          <div className={styles.ig}>
+            <img src={img} alt="" />
+          </div>
         </div>
         <div className={styles["course-type_right"]}>
           <div className={styles["course-type_right_top"]}>
@@ -24,7 +37,35 @@ const CourseType = () => {
             </p>
           </div>
           {/* bottom */}
-          <div className={styles["course-type_right_bottom"]}></div>
+          <div className={styles["course-type_right_bottom"]}>
+            {ASK_DATA.map(({ id, description, title }) => {
+              return (
+                <div
+                  key={id}
+                  onClick={() =>
+                    handleAccordion({
+                      id,
+                      active: isActive?.id === id ? isActive.active : false,
+                    })
+                  }
+                  className={`${styles["ask_accordion"]} ${isActive?.id === id && isActive.active ? styles.active : ""}`}>
+                  {/* top */}
+                  <div className={styles["ask_accordion_top"]}>
+                    <span>{title}</span>
+                    <div className={styles["arrow-up"]}>
+                      <RiArrowUpSLine />
+                    </div>
+                  </div>
+                  {/* bottom */}
+                  <div className={styles["details-wrapper"]}>
+                    <div className={styles["details-wrapper_inner"]}>
+                      <p>{description}</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/layout/Main/section-5/dataAsk.ts
@@ -0,0 +1,41 @@
+interface DataAsk {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
+
+export const ASK_DATA: DataAsk[] = [
+  {
+    id: 1,
+    img: "",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, unde!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dignissimos expedita dolorum quis, audio voluptate Temporary ratione ipsam totam. Necessitatibus.",
+  },
+  {
+    id: 2,
+    img: "",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, unde!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dignissimos expedita dolorum quis, audio voluptate Temporary ratione ipsam totam. Necessitatibus.",
+  },
+  {
+    id: 3,
+    img: "",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, unde!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dignissimos expedita dolorum quis, audio voluptate Temporary ratione ipsam totam. Necessitatibus.",
+  },
+  {
+    id: 4,
+    img: "",
+    title:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum, unde!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dignissimos expedita dolorum quis, audio voluptate Temporary ratione ipsam totam. Necessitatibus.",
+  },
+];
