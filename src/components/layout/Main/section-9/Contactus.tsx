cd5a565c41d4eb0ff80b36f95c3d8ce8a1b75deb
Hassan Alkhalaf <baraanas1521@gmail.com>
2026-06-26 22:48:41 +0300

    the end desgin

--- src/components/layout/Main/section-9/Contactus.tsx
+++ src/components/layout/Main/section-9/Contactus.tsx
@@ -1,4 +1,3 @@
-
 import Button from "@/components/ui/Button/Button";
 import styles from "./contact-us.module.scss";
 import { useScrollAnimation } from "@/hooks/useScrollAnimation";
@@ -6,19 +5,31 @@ import { useScrollAnimation } from "@/hooks/useScrollAnimation";
 type Props = {};
 
 const Contactus = (props: Props) => {
-    const [ref, isVisible] = useScrollAnimation();
+  const [ref, isVisible] = useScrollAnimation();
   return (
     <div className={styles["contact-us"]}>
-      <div ref={ref} className={`${styles["contact-us_container"]} ${isVisible ? styles.visible : ""}`}>
-        <h2>{"Bizden En Son Bilgilerle Güncel Kalın!".split(" ").map((ele) => (
+      <div
+        ref={ref}
+        className={`${styles["contact-us_container"]} ${isVisible ? styles.visible : ""}`}>
+        <h2
+          className={`${styles["heading"]} ${isVisible ? styles.visible : ""}`}>
+          {"Bizden En Son Bilgilerle Güncel Kalın!".split(" ").map((ele) => (
             <span>{ele}</span>
-        ))}</h2>
+          ))}
+        </h2>
         <div className={styles["box-email"]}>
-            <div className={styles["input-box"]}>
-            <input type="text" placeholder="e-postanı gir"/>
-
-            </div>
-            <Button style={{padding: "4px 20px", borderRadius: "calc(var(--border-radius-input-box) - 5px)"}} variant="white"> Gönder</Button>
+          <div className={styles["input-box"]}>
+            <input type="text" placeholder="e-postanı gir" />
+          </div>
+          <Button
+            style={{
+              padding: "4px 20px",
+              borderRadius: "calc(var(--border-radius-input-box) - 5px)",
+            }}
+            variant="white">
+            {" "}
+            Gönder
+          </Button>
         </div>
       </div>
     </div>
